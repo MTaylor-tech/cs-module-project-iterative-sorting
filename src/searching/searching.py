3092ac7501ed8a39550329395b2e8fe38ea235ea
Matthew Taylor <majikpig@gmail.com>
2020-09-02 20:14:08 -0400

    linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    for index, value in enumerate(arr):
+        if value == target:
+            return index
 
 
     return -1   # not found
